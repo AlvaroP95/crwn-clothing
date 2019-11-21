a1bc67302986d398a32cb7d16f0c5c75d02399cd
Alvaro Puertas <alvaropuertas1995@gmail.com>
2019-11-21 16:09:30 -0300

    s05 terminada, React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
 
-import HomePage from './pages/homepage/homepage.component';
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        {/* Switch renders only one of its children. With it there is no need to set a "exact" attribute. */}
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
