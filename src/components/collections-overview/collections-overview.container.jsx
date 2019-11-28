93721f3c6a15dc6dc46e39c109c01e3c7cf074c4
Alvaro Puertas <alvaropuertas1995@gmail.com>
2019-11-28 15:55:27 -0300

    S19 terminada. Cut the logic from shop component to a new CollectionContainer and CollectionsOverviewContainer. This new containers simply get the isLoading state and the Spinner and connects them to the Collection and CollectionOverview components. Now its easier to read the Shop component)

---
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -0,0 +1,23 @@
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectIsCollectionFetching } from "../../redux/shop/shop.selectors";
+import { compose } from "redux";
+import WithSpinner from "../with-spinner/with-spinner.component";
+import CollectionsOverview from "./collections-overview.component";
+
+const mapStateToProps = createStructuredSelector({
+  isLoading: selectIsCollectionFetching
+});
+
+const CollectionsOverviewContainer = compose(
+  connect(mapStateToProps),
+  WithSpinner
+)(WithSpinner(CollectionsOverview));
+
+/*
+  Equals to
+  const CollectionPageContainer = 
+  connect(mapStateToProps)(withSpinner(CollectionsOverview))
+*/
+
+export default CollectionsOverviewContainer;
---
+++ src/pages/collection/collection.container.jsx
@@ -0,0 +1,26 @@
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { createStructuredSelector } from "reselect";
+
+import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
+
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
+import CollectionPage from "./collection.component";
+import { selectIsCollectionsLoaded } from "../../redux/shop/shop.selectors";
+
+const mapStateToProps = createStructuredSelector({
+  isLoading: state => !selectIsCollectionsLoaded(state)
+});
+
+const CollectionPageContainer = compose(
+  connect(mapStateToProps),
+  WithSpinner
+)(CollectionPage);
+
+/*
+  Equals to
+  const CollectionPageContainer = 
+  connect(mapStateToProps)(WithSpinner(CollectionPage))
+*/
+
+export default CollectionPageContainer;
