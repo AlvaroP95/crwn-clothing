dfb0cd4980e65da9686d46daecb421b59254431b
Alvaro Puertas <alvaropuertas1995@gmail.com>
2019-12-08 19:52:37 -0300

    Trying to fix Heroku not showing the updated page

--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -42,12 +42,14 @@ const CheckoutPage = ({ cartItems, total }) => (
     {cartItems.map(cartItem => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
+    <p>X</p>
     <TotalContainer>TOTAL: ${total}</TotalContainer>
     <WarningContainer>
       *Please use the following test credit card for payments*
       <br />
       4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
     </WarningContainer>
+    X
     <StripeCheckoutButton price={total} />
   </CheckoutPageContainer>
 );
