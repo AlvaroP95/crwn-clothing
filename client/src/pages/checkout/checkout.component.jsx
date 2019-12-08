f120957081275cdb02c902bbcc53b3752a5a50db
Alvaro Puertas <alvaropuertas1995@gmail.com>
2019-12-08 20:03:57 -0300

    Trying to fix Heroku not updating code. 3

--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -49,7 +49,7 @@ const CheckoutPage = ({ cartItems, total }) => (
       <br />
       4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
     </WarningContainer>
-    X
+    XXXXX
     <StripeCheckoutButton price={total} />
   </CheckoutPageContainer>
 );
